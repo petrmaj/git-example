9e5a96188f1f7944dcfc4fbed3fa87efcceff69e
peta <peta.maj82@gmail.com>
2022-12-07 17:03:12 +0100

    Increments the loop variable when checking primes. Fixes issue #1.

--- main.cpp
+++ main.cpp
@@ -5,7 +5,7 @@ int main() {
     std::vector<int> result;
     for (int n = 2; n <= 100; ++n) {
         bool prime = true;
-        for (int i = 2; i < n; i) {
+        for (int i = 2; i < n; ++i) {
             if ((n % i) == 0) {
                 prime = false;
                 break;
